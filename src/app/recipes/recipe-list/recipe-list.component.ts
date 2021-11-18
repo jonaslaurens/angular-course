b9633a73a58f5d4f481be8a9852bc170ef0f1187
jonaslaurens <lauwe505@gmail.com>
2021-11-18 16:10:50 +0100

    project setup - The Basics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../interfaces/recipe.interface';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    {
+      name: 'A test Recipe',
+      description: 'This is simply a test',
+      imagePath:
+        'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
+    },
+    {
+      name: 'Another Recipe',
+      description: 'This is simply a test',
+      imagePath:
+        'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
